e3ab334b8a84d31a5ab32d2cdd0c908ee3102c64
Zavan7 <vitorzavanj@gmail.com>
2025-01-11 00:12:41 -0300

    att

--- aula_poo/aula_poo_a.py
+++ aula_poo/aula_poo_a.py
@@ -1,4 +1,7 @@
-CAMINHO_ARQUIVO = 'aula_poo.json'
+import json
+from pathlib import Path
+
+CAMINHO_ARQUIVO = Path(__file__).parent / 'aula_poo.json'
 
 class Pessoa:
     def __init__(self, nome, idade):
@@ -11,3 +14,12 @@ p2 = Pessoa('João', 30)
 p3 = Pessoa('Maria', 40)
 p4 = Pessoa('Eustacio', 35)
 p5 = Pessoa ('Genoveva', 29)
+bd = [vars(p) for p in [p1, p2, p3, p4, p5]]
+
+def dump():
+    with open(CAMINHO_ARQUIVO, 'w') as f:
+        json.dump(bd, f, ensure_ascii=False, indent=2)
+        print('Sucess')
+
+if __name__ == '__main__':
+    dump()
