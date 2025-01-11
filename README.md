a12e3ae164626720d8220a0b0a7e68801f2c3885
Zavan7 <vitorzavanj@gmail.com>
2025-01-10 23:25:29 -0300

    att

--- README.md
+++
@@ -1,6 +0,0 @@
-# Python
-
-	![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
-
-
-## Vamos estudar Python?
---
+++ aula_poo/aula_poo_a.py
@@ -0,0 +1,13 @@
+CAMINHO_ARQUIVO = 'aula_poo.json'
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+
+p1 = Pessoa('Vitor', 20)
+p2 = Pessoa('João', 30)
+p3 = Pessoa('Maria', 40)
+p4 = Pessoa('Eustacio', 35)
+p5 = Pessoa ('Genoveva', 29)
