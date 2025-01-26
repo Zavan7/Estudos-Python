a6a7f9b3794a598681e65797d3e633bca95b63c0
Zavan7 <vitorzavanj@gmail.com>
2025-01-26 14:25:01 -0300

    Refactor Pessoa class to include sobrenome and nome_completo property; update main script for testing

--- aula_poo/aula_poo_33.py
+++ aula_poo/aula_poo_33.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
+@dataclass(init=False)
 class Pessoa:
     nome: str
     sobrenome: str
@@ -12,14 +13,24 @@ class Pessoa:
     @nome_completo.setter
     def nome_completo(self, valor):
         nome, *sobrenome = valor.split()
+    def __init__(self, nome, sobrenome):
         self.nome = nome
         self.sobrenome = ' '.join(sobrenome)
+        self.sobrenome = sobrenome
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+    def __post_init__(self):
+        print('POST INIT')
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
+    # @nome_completo.setter
+    # def nome_completo(self, valor):
+    #     nome, *sobrenome = valor.split()
+    #     self.nome = nome
+    #     self.sobrenome = ' '.join(sobrenome)
 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 'Otávio')
-    p2 = Pessoa('Luiz', 'Otávio')
-    print(p1 == p2)
     p1 = Pessoa('Luiz', 'Otávio')
     p1.nome_completo = 'Maria Helena'
     print(p1)
